19e53e252807bccc7397d94f05a14faa980ee0b9
Anand <sharan.anand@astrotalk.com>
2024-09-20 18:25:34 +0530

    landing page modification

--- src/app/landing/home/home.component.ts
+++ src/app/landing/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  timeRemaining: string = '10:00:00';
 
+  ngOnInit(): void {
+    this.startTimer();
+  }
+
+  startTimer() {
+    const endTime = new Date('2024-12-31T23:59:59').getTime();
+    const timerInterval = setInterval(() => {
+      const currentTime = new Date().getTime();
+      const timeLeft = endTime - currentTime;
+
+      if (timeLeft < 0) {
+        clearInterval(timerInterval);
+        this.timeRemaining = 'EXPIRED';
+        return;
+      }
+
+      const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+      this.timeRemaining = `${days}:${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }, 1000);
+  }
 }
